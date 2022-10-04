a5ac5b4894b02ccf5d61b6c542db86d4dd1b84ff
Matheus Barboza <matheus.barboza0@gmail.com>
2022-10-04 16:11:40 -0300

    adicionando Vue e Vue Router

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- 
+ -->
+
+ <template>
+  <nav>
+    <RouterLink to="/hello">Hello</RouterLink>
+  </nav>
+  <router-view/>
+ </template>
+ 
+ <script>
+ export default {
+ 
+ }
+ </script>
+ 
+ <style>
+ 
+ </style>
